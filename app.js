1812f4882a0fe58d773c6bedc0f6043d9373929e
Sereah <sereahha@gmail.com>
2024-09-26 22:32:20 +0800

    [feature]初步实现点击税后卡片跳转
    1.格式化代码
    2.转移到vscode开发
    3.新增界面yearDetail

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // app.js
 App({
-  globalData: {
-    __vBeforeSalary: "",
-    __vBeforeProvidentFund: "",
-    __vBeforeProvidentFundPer: "",
-    __vBeforeSocial: "",
-    __vBeforeBonus: "",
-    __vBeforeBonusMonth: "",
+    globalData: {
+        __vBeforeSalary: "",
+        __vBeforeProvidentFund: "",
+        __vBeforeProvidentFundPer: "",
+        __vBeforeSocial: "",
+        __vBeforeBonus: "",
+        __vBeforeBonusMonth: "",
 
-    __vAfterSalary: "",
-    __vAfterProvidentFund: "",
-    __vAfterProvidentFundPer: "",
-    __vAfterSocial: "",
-    __vAfterBonus: "",
-    __vAfterBonusMonth: "",
+        __vAfterSalary: "",
+        __vAfterProvidentFund: "",
+        __vAfterProvidentFundPer: "",
+        __vAfterSocial: "",
+        __vAfterBonus: "",
+        __vAfterBonusMonth: "",
 
-    __vTax: "",
-    __vOtherTax: "",
-  }
+        __vTax: "",
+        __vOtherTax: "",
+    }
 })
